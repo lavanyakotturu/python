116f1e734b619b873a10f297d3983152b22d2f8c
lavanyakotturu <kotturulavanya41@gmail.com>
2025-01-05 13:30:42 +0530

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -6,65 +6,64 @@ print(type(a),type(b))
 '''
 
 
-'''
+
 a=50.5551
 b=0.004
 print(type(a+b))
-'''
 
 
-'''
+
+
 a="lavanya"
 b="binnu"
 print(type(a))
-'''
+
 
 
 #x=10+20j
 #print(type(x))
 
 
-'''
+
 a=["blue,red,green"]
 print(type(a))
-'''
 
 
 
-'''
+
+
 p=("banana,apple,orange")
 print(type(p))
-'''
 
-'''
+
+
 x=range(10)
 print(x)
 print(type(x))
-'''
 
 
-'''
+
+
 a={12,34,56}
 print(type(a))
-'''
 
 
-'''
+
+
 a={"name":"harsha","age":"3"}
 print(type(a))
-'''
 
-'''
+
 a={10,237,34}
 print(type(a))
-'''
 
 
-'''
+
+
 a=100
 b=199
 print(b>a)
 print(type(b<a))
-'''
+
 
 
